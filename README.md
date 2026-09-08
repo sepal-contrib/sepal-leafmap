6cb1891dba7fc0acaaa0734dc24cd56813123008
dfguerrerom <dfgm2006@gmail.com>
2026-09-08 10:17:35 +0200

    fix: replace key-gated CARTO basemap with Esri canvas
    CARTO now answers keyless tile requests with HTTP 200 and an
    "API KEY REQUIRED" watermark, so the map rendered unusable.
    
    Also drop the unused component stub packages (model, scripts, widget,
    parameter), the stray .github/github/workflows/lint.yml that GitHub
    never picked up, and the translation scratch notebook.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # sepal leafmap
 
-[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://github.com/12rambau/sepal-leafmap/blob/master/LICENSE)
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://github.com/sepal-contrib/sepal-leafmap/blob/main/LICENSE)
 [![Black badge](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/psf/black)
 
 # About
--- component/tile/map_tile.py
+++ component/tile/map_tile.py
@@ -1,10 +1,19 @@
 """The map displayed in the map application."""
 
 import ipyvuetify as v
-from ipyleaflet import FullScreenControl, basemaps
+from ipyleaflet import FullScreenControl, TileLayer
 from leafmap.leafmap import Map
 from sepal_ui import mapping as sm
 from sepal_ui import sepalwidgets as sw
+
+# CARTO answers keyless requests with HTTP 200 and an "API KEY REQUIRED" watermark,
+# which is why the Positron/DarkMatter pair this app used to rely on is unusable.
+# Esri's canvas basemaps are the closest keyless equivalent, but they only carry
+# global data to zoom 16 and serve a placeholder past it, hence max_native_zoom.
+ESRI_CANVAS_URL = (
+    "https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/"
+    "World_{}_Gray_Base/MapServer/tile/{{z}}/{{y}}/{{x}}"
+)
 
 
 class MapTile(sw.Tile):
@@ -13,13 +22,14 @@ class MapTile(sw.Tile):
 
         Use this map to gather all your widget and place them on it. It will reduce the amount of work to perform in the notebook
         """
-        # create a map
-        default_basemap = (
-            basemaps.CartoDB.DarkMatter
-            if v.theme.dark is True
-            else basemaps.CartoDB.Positron
+        basemap = TileLayer(
+            url=ESRI_CANVAS_URL.format("Dark" if v.theme.dark else "Light"),
+            name="Esri Gray Canvas",
+            attribution="Tiles &copy; Esri",
+            max_native_zoom=16,
+            max_zoom=24,
         )
-        self.m = Map(basemap=default_basemap, zoom=3)
+        self.m = Map(basemap=basemap, zoom=3)
         self.m._id = "leafmap"
         self.m.add_class(self.m._id)
 
@@ -31,5 +41,3 @@ class MapTile(sw.Tile):
 
         # create the tile
         super().__init__("map_tile", "", [self.m])
-
-        return
